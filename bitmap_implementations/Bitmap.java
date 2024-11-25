45d493bb8d3b31ed7e0f957f57750d207be1f26d
JJwu <74655163+jiaojiaowujj@users.noreply.github.com>
2024-11-25 16:04:23 +0800

    Update Bitmap.java

--- bitmap_implementations/Bitmap.java
+++ bitmap_implementations/Bitmap.java
@@ -16,12 +16,20 @@ limitations under the License.
 
 package bitmap_implementations;
 
-public abstract class Bitmap {
+public abstract class Bitmap { //抽象类
 	
 	public abstract long size();
 	public abstract void set(long bit_index, boolean value);
+	//设置位向量中指定位置的值
+	//bit_index 表示需要设置的位的索引（从 0 开始计数）
+	//value 指定该位应设置为的值：true 表示 1，false 表示 0
+	
 	public abstract void setFromTo(long from, long to, long value);
 	public abstract boolean get(long bit_index);
+	// 获取位向量中指定位置的值
+	// bit_index 表示需要查询的位的索引
+	//返回一个布尔值：true：表示该位为 1, false：表示该位为 0
+	
 	public abstract long getFromTo(long from, long to);
 	
 	public static boolean get_fingerprint_bit(long index, long fingerprint) {
